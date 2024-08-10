a6635e786e4155d71cc677b5eee873e59ff2be21
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2024-08-10 23:13:03 +0200

    demonstration of getMoves

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include "movegen.c"
-
 int main() {
+    setbuf(stdout, NULL); /*disable buffering for stdout*/
+    setbuf(stdin, NULL); /*same for stdin*/
     struct Piece* ps = loadpieces(); /*initalize pointer to pieces array allocated on heap*/
     struct Move move;
-    move.startX = 3;
+    move.startX = 6;
     move.startY = 1;
-    move.destX = 3;
-    move.destY = 1;
-    move.pieceID = 12;
-    printf("%d", mValid(move, ps, 0));
+    move.destX = 4;
+    move.destY = 3;
+    move.pieceID = 13;
+    struct Move* mvs = getMoves(ps, 0);
+    printf("\nARLEN: %d\n", mvs[0].arlen); 
   //mValid(move, ps);
+   // printf("%d", mValid(move, ps, 0));
+
+  //  printf("EVAL: %d\n", eval(ps, 0));
     
     return 0;
 }
