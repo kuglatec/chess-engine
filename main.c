2ffc8a816de520fd381e92647fd77a990f5da2e2
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2024-08-12 00:09:48 +0200

    added testing code for makemove and getmoves

--- main.c
+++ main.c
@@ -4,14 +4,16 @@ int main() {
     setbuf(stdin, NULL); /*same for stdin*/
     struct Piece* ps = loadpieces(); /*initalize pointer to pieces array allocated on heap*/
     struct Move move;
-    move.startX = 6;
-    move.startY = 1;
-    move.destX = 4;
+    move.startX = 1;
+    move.startY = 2;
+    move.destX = 1;
     move.destY = 3;
-    move.pieceID = 13;
+    move.pieceID = 0;
     struct Move* mvs = getMoves(ps, 0);
-    printf("\nARLEN: %d\n", mvs[0].arlen); 
+    //printf("\nARLEN: %d\n", mvs[0].arlen); 
   //mValid(move, ps);
+   // makeMove(move, ps, 0);
+    printf("\n%d/%d", ps[0].xpos, ps[0].ypos);
    // printf("%d", mValid(move, ps, 0));
 
   //  printf("EVAL: %d\n", eval(ps, 0));
