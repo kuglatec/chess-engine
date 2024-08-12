d0a3f390c38ec642980b36500cb7be60d8bcedb4
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2024-08-13 00:46:52 +0200

    my testing playground

--- main.c
+++ main.c
@@ -9,11 +9,19 @@ int main() {
     move.destX = 1;
     move.destY = 3;
     move.pieceID = 0;
-    struct Move* mvs = getMoves(ps, 0);
+  //  struct Move* mvs = getMoves(ps, 0);
+    struct State* states = getstates(ps, 0, 0);
+ //   printf("\nL:%d\n", states[0].m.destY);
+   // printf("\n%d\n", states[0].score);
+   
+    printf("\n%d\n", eval(states[0].ps, 0));
+    for (int p = 0; p < 32; p++) {
+      printf("\nX:%d\nY:%d\n", states[0].ps[p].xpos, states[0].ps[p].ypos);
+   }
     //printf("\nARLEN: %d\n", mvs[0].arlen); 
   //mValid(move, ps);
    // makeMove(move, ps, 0);
-    printf("\n%d/%d", ps[0].xpos, ps[0].ypos);
+ //   printf("\n%d/%d", ps[0].xpos, ps[0].ypos);
    // printf("%d", mValid(move, ps, 0));
 
   //  printf("EVAL: %d\n", eval(ps, 0));
